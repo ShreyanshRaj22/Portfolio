aac3f33854f1f4dc6603a7496edf9f8b861c00df
Shreyansh Raj <211230051@nitdelhi.ac.in>
2023-11-22 08:42:59 +0530

    Link update

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -45,7 +45,7 @@ export const Footer = () => {
             <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
               <p>
-                <a href='#!' className='text-reset'>
+                <a href='https://drive.google.com/file/d/1vLYWKh2rG5njaBJDwuifLgxlaM9XXrSI/view?usp=drive_link' className='text-reset'>
                   Resume
                 </a>
               </p>
