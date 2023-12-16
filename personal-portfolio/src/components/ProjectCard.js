3497525c0c887cd8243b9e6e36e32222ac927755
Shreyansh Raj <211230051@nitdelhi.ac.in>
2023-12-16 08:32:25 +0530

    Project Card update

--- personal-portfolio/src/components/ProjectCard.js
+++ personal-portfolio/src/components/ProjectCard.js
@@ -3,15 +3,13 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgUrl, siteUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx" style={{width: "350px", height: "170px", backgroundSize: "contain"}}>
-      <a href={siteUrl}>
+      <a href={siteUrl} className="proj-imgbx">
         <img src={imgUrl} className="img-fluid" />
-        </a>
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
-      </div>
+      </a>
     </Col>
-  )
-}
+  );
+};
